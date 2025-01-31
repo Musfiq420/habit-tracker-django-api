5d3116a03c4a8f73e221c3f0bd260e2682bbc009
Md. Musfiqur Rahman <mrifat46@gmail.com>
2025-01-31 16:19:49 +0600

    added postgres

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -95,10 +95,22 @@ WSGI_APPLICATION = 'habit_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'habit-tracker',
+        'HOST': 'ep-weathered-sound-a4qqlgq1.us-east-1.aws.neon.tech',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'AuSTqEoWL36b',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
